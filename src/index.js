9310fce10ecc4c154a7f64524863535e011c7d05
AllanOliveiraM <nallamo2016@gmail.com>
2020-06-30 15:07:08 -0300

    Add init template bootstrapped with create-react-app, and basics settings.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+import 'styles/index.css'
+
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
