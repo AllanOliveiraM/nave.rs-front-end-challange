e8d9130805588bc0bab7f5d01cf4d8b00f00c295
AllanOliveiraM <nallamo2016@gmail.com>
2020-07-04 19:12:10 -0300

    Render Test

---
+++ src/app.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/index.css'
+
+import AppComponents from './App'
+
+it('renders without crashing', () => {
+  const divLoadBar = document.createElement('div');
+  const divApp = document.createElement('div');
+  ReactDOM.render(
+    <AppComponents.LoadBar />,
+    divLoadBar
+  )
+
+  ReactDOM.render(
+    <AppComponents.App />,
+    divApp
+  )
+});
