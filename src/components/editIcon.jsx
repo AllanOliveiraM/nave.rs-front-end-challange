c30e45284ea5f9de08c9580bcb5d3f0fe5c4f970
AllanOliveiraM <nallamo2016@gmail.com>
2020-07-05 01:17:25 -0300

    Icons size changed.

--- src/components/editIcon.jsx
+++ src/components/editIcon.jsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 function EditIcon(props) {
   return (
-    <svg width={24} height={24} viewBox="0 0 24 24" fill="none" {...props}>
+    <svg className='zoom-in edit-icon' width={28} height={28} viewBox="0 0 24 24" fill="none" {...props}>
       <path
         d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM21.41 6.34l-3.75-3.75-2.53 2.54 3.75 3.75 2.53-2.54z"
         fill="#212121"
