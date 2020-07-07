77caa488b3ebbcaa1539f91e07d9d73d53b0f795
AllanOliveiraM <nallamo2016@gmail.com>
2020-07-07 14:29:31 -0300

    Arrow Back SVG Icon added

---
+++ src/components/arrowBack.jsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+function ArrowBack(props) {
+  return (
+    <svg width={13} height={20} viewBox="0 0 13 20" fill="none" {...props}>
+      <path
+        d="M12.51 1.87L10.73.1.84 10l9.9 9.9 1.77-1.77L4.38 10l8.13-8.13z"
+        fill="#000"
+      />
+    </svg>
+  )
+}
+
+export default ArrowBack
