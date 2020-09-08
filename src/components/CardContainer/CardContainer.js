a79c2c41b37d520894b6c189601569ef441e61a3
AllanOliveiraM <nallamo2016@gmail.com>
2020-09-08 09:03:03 -0300

    refactor(cardcontainer): changed length for array
    The lenth validation was changed from React array to Cards array.

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -7,15 +7,13 @@ import Card from 'components/Card'
 import { CURRENT_LANGUAGE as lang } from 'helpers/constants'
 
 const CardContainerComponent = ({ cards, loading, rowProps, columnProps, ...props }) => {
-  const cardsRender = cards.map(card => (
-    <CardColumn key={card?.id} {...columnProps} className='col col-2 col-3 col-4'>
-      <Card className='in-animation' card={card} />
-    </CardColumn>
-  ))
-
   const CardContent = () => {
-    if (cardsRender.length) {
-      return cardsRender
+    if (cards.length) {
+      return cards.map(card => (
+        <CardColumn key={card.id} {...columnProps} className='col col-2 col-3 col-4'>
+          <Card className='in-animation' card={card} />
+        </CardColumn>
+      ))
     } else {
       return <CardMessage>{lang.pageComposition.messages.noCards}</CardMessage>
     }
